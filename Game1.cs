0caeb06b7cb8f76845bedc0cb8e95b3c035b7458
Emma-Shifter <n.ba.04@mail.ru>
2023-04-28 19:00:02 +0500

    Add logic for start button

--- Game1.cs
+++ Game1.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using sakurario.Controls;
 using SharpDX.Direct2D1;
 using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
+using sakurario.States;
 
 namespace sakurario
 {
@@ -15,6 +16,14 @@ namespace sakurario
         SpriteBatch spriteBatch;
         Texture2D background;
         private List<Component> _gameComponents;
+
+        private State _currentState;
+        private State _nextState;
+
+        public void ChangeState(State state)
+        {
+            _nextState = state;
+        }
 
         public Game1()
         {
@@ -35,39 +44,19 @@ namespace sakurario
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             background = Content.Load<Texture2D>("background");
-
-            var quitButton = new Button(Content.Load<Texture2D>("Controls/quit_button"))
-            {
-                Position = new Vector2(410, 590),
-            };
-            quitButton.Click += QuitButton_Click;
-
-            var startButton = new Button(Content.Load<Texture2D>("Controls/start_button"))
-            {
-                Position = new Vector2(390, 420),
-            };
-            startButton.Click += StartButton_Click;
-
-            _gameComponents = new List<Component>()
-            {
-                startButton,
-                quitButton,
-            };
+            _currentState = new MenuState(this, graphics.GraphicsDevice, Content);
         }
 
-        private void QuitButton_Click(object sender, System.EventArgs e)
-        {
-            Exit();
-        }
-        private void StartButton_Click(object sender, System.EventArgs e)
-        {
-            var index = 0;
-        }
         protected override void Update(GameTime gameTime)
         {
-            foreach (var component in _gameComponents)
-                component.Update(gameTime);
+            if (_nextState != null)
+            {
+                _currentState = _nextState;
+                _nextState = null;
+            }
 
+            _currentState.Update(gameTime);
+            _currentState.PostUpdate(gameTime);
             base.Update(gameTime);
         }
 
@@ -76,8 +65,7 @@ namespace sakurario
             GraphicsDevice.Clear(Color.White);
             spriteBatch.Begin();
             spriteBatch.Draw(background, new Rectangle(0, 0, 1920, 1050), Color.White);
-            foreach (var component in _gameComponents)
-                component.Draw(gameTime, spriteBatch);
+            _currentState.Draw(gameTime, spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
         }
---
+++ States/MenuState.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sakurario.Controls;
+
+namespace sakurario.States
+{
+    public class MenuState : State
+    {
+        private List<Component> _components;
+
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+          : base(game, graphicsDevice, content)
+        {
+            var startButtonTexture = _content.Load<Texture2D>("Controls/start_button");
+            var quitButtonTexture = _content.Load<Texture2D>("Controls/quit_button");
+
+            var startButton = new Button(startButtonTexture)
+            {
+                Position = new Vector2(390, 420),
+            };
+            startButton.Click += StartButton_Click;
+
+            var quitButton = new Button(quitButtonTexture)
+            {
+                Position = new Vector2(410, 590),
+            };
+            quitButton.Click += QuitButton_Click;
+
+            _components = new List<Component>()
+            {
+                startButton,
+                quitButton,
+            };
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (var component in _components)
+                component.Draw(gameTime, spriteBatch);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            _game.ChangeState(new GameState(_game, _graphicsDevice, _content));
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            // remove sprites if they're not needed
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+                component.Update(gameTime);
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            _game.Exit();
+        }
+    }
+}
