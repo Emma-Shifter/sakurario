0637058db3e1fe4d4eab84582f3c060898a5b458
Emma-Shifter <n.ba.04@mail.ru>
2023-04-30 15:18:25 +0500

    add steps ro right and left

--- States/GameState.cs
+++ States/GameState.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
+using sakurario.Sprites;
+using sakurario.Models;
 
 namespace sakurario.States
 {
@@ -13,16 +17,38 @@ namespace sakurario.States
     {
         //private List<Component> _components;
         Texture2D background;
+        private List<Sprite> _sprites;
         public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
           : base(game, graphicsDevice, content)
         {
-            background = _content.Load<Texture2D>("test_bg");
+            background = _content.Load<Texture2D>("platform_bg");
+            var animations = new Dictionary<string, Animation>(){
+                {"WalkRight", new Animation(_content.Load<Texture2D>("Player/playerstepright"), 4) },
+                {"WalkLeft", new Animation(_content.Load<Texture2D>("Player/playerstepleft"), 4) },
+                //{"JumpRight", new Animation(_content.Load<Texture2D>("Player/playerjumpright"), 4) },
+                //{"JumpLeft", new Animation(_content.Load<Texture2D>("Player/playerjumpleft"), 4) },
+            };
+            _sprites = new List<Sprite>()
+            {
+                new Sprite(animations)
+                {
+                    Position = new Vector2(100, 100),
+                    Input = new Input()
+                    {
+                        Right = Keys.D,
+                        Left = Keys.A,
+                    }
+                }
+            };
+
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             spriteBatch.Draw(background, new Rectangle(0, 0, 1920, 1050), Color.White);
+            foreach (var sprite in _sprites)
+                sprite.Draw(spriteBatch);
             spriteBatch.End();
         }
 
@@ -33,7 +59,10 @@ namespace sakurario.States
 
         public override void Update(GameTime gameTime)
         {
-
+            foreach (var sprite in _sprites)
+            {
+                sprite.Update(gameTime, _sprites);
+            }
         }
     }
 }
